c9a8d5ec58af38b52442e2b93f23e7ff66b7604c
Nati Martinez <natividad.martinez@reutlingen-university.de>
2023-12-10 23:28:28 +0100

    Some tests and changes

--- pages/api/list-pets.js
+++ pages/api/list-pets.js
@@ -2,10 +2,11 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(request, response) {
   try {
-    const result =
-      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-    return response.status(200).json({ result });
+    const pets = await sql`SELECT * FROM Pets;`;
+    return response.status(200).json(pets.rows );
   } catch (error) {
     return response.status(500).json({ error });
   }
+ 
+ 
 }
