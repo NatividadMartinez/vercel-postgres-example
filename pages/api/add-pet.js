b47d5e81a187f5147451859433a106ce2109475c
Nati Martinez <natividad.martinez@reutlingen-university.de>
2023-12-11 01:22:20 +0100

    next change

--- pages/api/add-pet.js
+++ pages/api/add-pet.js
@@ -3,16 +3,11 @@ import { sql } from '@vercel/postgres';
 export default async function handler(request, response) {
   try {
     const { petName, ownerName } = JSON.parse(request.body);
-    //const petName = request.query.petName;
-    //const ownerName = request.query.ownerName;
     if (!petName || !ownerName) throw new Error('Pet and owner names required');
     await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
     const pets = await sql`SELECT * FROM Pets;`;
-    return response.status(200).json({ pets: pets.rows });
+    return response.status(200).json(pets.rows);
   } catch (error) {
     return response.status(500).json({ error });
   }
- 
-  //const pets = await sql`SELECT * FROM Pets;`;
-  //return response.status(200).json({ pets });
 }
